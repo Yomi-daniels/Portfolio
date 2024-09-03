9bc4cbf320c6ca4656344bf481f0d460e31cdb00
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-09-03 08:45:04 +0100

    first commit

---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick: () => void;
+  otherClasses?: string;
+}) => {
+  return (
+    // Button code
+    <button
+      className={`w-full inline-flex h-12 animate-shimmer items-center justify-center rounded-lg border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none md:w-60 md:mt-10 gap-2  ${otherClasses} hover:text-white-100`}
+    >
+      {position === "left" && icon}
+      {title}
+      {position === "right" && icon}
+    </button>
+  );
+};
+
+export default MagicButton;
