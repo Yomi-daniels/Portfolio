790abe93439d8c1f794cd560c572c0c7c87fcbc1
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-12-04 15:08:14 +0100

    added icons

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -8,6 +8,13 @@ import {
 } from "framer-motion";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import {
+  FaBars,
+  FaHome,
+  FaPhone,
+  FaProjectDiagram,
+  FaRProject,
+} from "react-icons/fa"; // Import a mobile menu icon
 
 export const FloatingNav = ({
   navItems,
@@ -25,7 +32,6 @@ export const FloatingNav = ({
   const [visible, setVisible] = useState(false);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -56,10 +62,22 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit w-full fixed top-10 inset-x-0 mx-auto border rounded-lg  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
+          "flex max-w-fit w-full fixed top-10 inset-x-0 mx-auto border rounded-lg shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100 max-sm:px-24 max-sm:py-7",
           className
         )}
       >
+        {/* Mobile menu icon */}
+        <div className="hidden max-sm:flex absolute items-center gap-12 justify-between">
+          <button className="sm:hidden text-neutral-600 dark:text-neutral-50 hover:text-neutral-500 dark:hover:text-neutral-300">
+            <FaHome size={20} />
+          </button>
+          <button className="sm:hidden text-neutral-600 dark:text-neutral-50 hover:text-neutral-500 dark:hover:text-neutral-300">
+            <FaPhone size={20} />
+          </button>
+          <button className="sm:hidden text-neutral-600 dark:text-neutral-50 hover:text-neutral-500 dark:hover:text-neutral-300">
+            <FaProjectDiagram size={20} />
+          </button>
+        </div>
         {navItems.map((navItem: any, idx: number) => (
           <Link
             key={`link=${idx}`}
@@ -68,8 +86,8 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="block sm:hidden">{navItem.icon}</span>
           </Link>
         ))}
       </motion.div>
